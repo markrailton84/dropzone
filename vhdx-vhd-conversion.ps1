13a90327b620e5800c615c5f6eea4df09320907f
Mark Railton <markjasonrailton@gmail.com>
2021-07-14 14:14:39 +0100

    added: pre-reqs

--- vhdx-vhd-conversion.ps1
+++ vhdx-vhd-conversion.ps1
@@ -3,6 +3,15 @@
 # 1. Convert VHDX to VHD
 # 2. Upload VHD to Storage Account - Blob - Container
 
+# pre-reqs
+
+# Install the AzureRM.BootStrapper module. Select Yes when prompted to install NuGet
+Install-Module -Name AzureRM.BootStrapper
+
+# Install and import the API Version Profile required by Azure Stack Hub into the current PowerShell session.
+Use-AzureRmProfile -Profile 2019-03-01-hybrid -Force
+Install-Module -Name AzureStack -RequiredVersion 1.8.3
+
 # variable delcared
 
 $rgName = "resourceGroupName"
